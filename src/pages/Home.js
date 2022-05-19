316c00936a8db112b3d0fb6ab7860f0e2c53f981
jtpurp <ruppjt@gmail.com>
2022-05-19 14:24:56 -0400

    Responsive Update 1.3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,23 +3,21 @@ import '../styles/Home.css';
 
 function Home() {
     return (
-        <div className="home-flex">
-            <div className="home-container">
-                <h1 id="hello">H E L L O</h1>
-                <div id="my-name">
-                    <span>My name is</span><span className="bio-word" id="jt"> JT Rupp </span><span>(he/they)</span>
+        <div className="home-container">
+            <h1 id="hello">H E L L O</h1>
+            <div id="my-name">
+                <span>My name is</span><span className="bio-word" id="jt"> JT Rupp </span><span>(he/they)</span>
+            </div>
+            <span id="small-text">and I'm an</span>
+            <div className="bio-container">
+                <div>
+                    <span id="bio" className="no-show">⚙️</span><span className="bio-word" id="bio">Engineer</span><span className="show" id="bio">⚙️</span>
                 </div>
-                <span id="small-text">and I'm an</span>
-                <div className="bio-container">
-                    <div>
-                        <span id="bio" className="no-show">⚙️</span><span className="bio-word" id="bio">Engineer</span><span className="show" id="bio">⚙️</span>
-                    </div>
-                    <div>
-                        <span id="bio" className="no-show">🌀</span><span className="bio-word" id="bio">Flow Artist</span><span className="show" id="bio">🌀</span>
-                    </div>
-                    <div>
-                        <span id="bio" className="no-show">🎶</span><span className="bio-word" id="bio">Music Enthusiast</span><san className="show" id="bio">🎶</san>
-                    </div>
+                <div>
+                    <span id="bio" className="no-show">🌀</span><span className="bio-word" id="bio">Flow Artist</span><span className="show" id="bio">🌀</span>
+                </div>
+                <div>
+                    <span id="bio" className="no-show">🎶</span><span className="bio-word" id="bio">Music Enthusiast</span><san className="show" id="bio">🎶</san>
                 </div>
             </div>
         </div>
