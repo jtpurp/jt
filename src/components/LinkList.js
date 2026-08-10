694b48ec7a57428e2afdc0911c07cd24e262b9e9
JT <ruppjt@gmail.com>
2026-08-10 18:20:30 -0400

    Updated homepage formatting so text is more visible and reorganized link list for mobile view

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -10,8 +10,8 @@ function LinkList() {
                 <Link to="/experience" className="listed-link" id="link">EXPERIENCE</Link>
                 <Link to="/education" className="listed-link" id="link">EDUCATION</Link>
                 <Link to="/portfolio" className="listed-link" id="link">PORTFOLIO</Link>
-                <Link to="/flow-arts" className="listed-link" id="link">FLOW ARTS</Link>
                 <Link to="/travel" className="listed-link" id="link">TRAVEL</Link>
+                <Link to="/flow-arts" className="listed-link" id="link">FLOW ARTS</Link>
                 <Link to="/music" className="listed-link" id="link">MUSIC</Link>
             </div>
         </div>
